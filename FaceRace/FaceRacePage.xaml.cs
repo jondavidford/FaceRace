df4304718529448fc3c597ccc3a9f06ab7c5ca4a
Jon Ford <jondavidford@gmail.com>
2018-03-31 13:41:05 -0400

    initial commit, drawing a black circle

---
+++ FaceRace/FaceRacePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace FaceRace
+{
+    public partial class FaceRacePage : ContentPage
+    {
+        public FaceRacePage()
+        {
+            InitializeComponent();
+        }
+
+        private void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs eventArgs)
+        {
+            SKSurface surface = eventArgs.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.CornflowerBlue);
+
+            canvas.DrawCircle(0, 0, 200, new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.Black });
+        }
+    }
+}
